26a346b87cfe5aa0159f15fa74f6317b96e5342b
aminerwx <aminerwx@gmail.com>
2024-03-24 02:56:28 +0000

    feat: added linkedlist methods
    ll_remove_begin and ll_remove_end

--- linkedlist/linkedlist.c
+++ linkedlist/linkedlist.c
@@ -5,6 +5,9 @@
 node_t *ll_create(void) { return (node_t *)calloc(1, sizeof(node_t)); }
 
 void ll_print(node_t *head) {
+  if (head == NULL) {
+    return;
+  }
   int i = 0;
   node_t *current = head;
   while (current != NULL) {
@@ -19,6 +22,7 @@ void ll_free(node_t *head) {
     tmp = head;
     head = head->next;
     free(tmp);
+    tmp = NULL;
   }
 }
 
@@ -26,22 +30,51 @@ void ll_insert_begin(node_t **head, char *data) {
   node_t *new = ll_create();
   new->data = data;
   new->next = *head;
+  new->size = (*head)->size + 1;
   *head = new;
 }
 
 void ll_insert_end(node_t *head, char *data) {
   if (head->data == NULL) {
     head->data = data;
+    head->size++;
   } else {
     node_t *current = head;
     while (current->next != NULL) {
       current = current->next;
     }
-    current->next = (node_t *)malloc(sizeof(node_t));
+    current->next = ll_create();
     if (current == NULL) {
       return;
     }
     current->next->data = data;
-    current->next->next = NULL;
+    head->size++;
   }
+}
+
+void ll_remove_end(node_t *head) {
+  if (head->next == NULL) {
+    head->data = NULL;
+    head->size = 0;
+    return;
+  }
+  node_t *current = head;
+  while (current->next->next != NULL) {
+    current = current->next;
+  }
+  free(current->next);
+  current->next = NULL;
+  head->size--;
+}
+
+void ll_remove_begin(node_t **head) {
+  if ((*head)->next == NULL) {
+    (*head)->data = NULL;
+    (*head)->size = 0;
+    return;
+  }
+  node_t *tmp = *head;
+  *head = (*head)->next;
+  (*head)->size = --tmp->size;
+  free(tmp);
 }
--- linkedlist/linkedlist.h
+++ linkedlist/linkedlist.h
@@ -1,5 +1,6 @@
 typedef struct node {
   char *data;
+  unsigned size;
   struct node *next;
 } node_t;
 
@@ -8,3 +9,5 @@ void ll_print(node_t *head);
 void ll_free(node_t *head);
 void ll_insert_begin(node_t **head, char *data);
 void ll_insert_end(node_t *head, char *data);
+void ll_remove_end(node_t *head);
+void ll_remove_begin(node_t **head);
